73acc4cac3280689d1c478c0c89f48ceb298f274
trandinhvu13 <vudigger@gmail.com>
2019-12-06 21:53:01 +0700

    Init

---
+++ SentryVR/Assets/Scripts/Blaster.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Blaster : MonoBehaviour
+{
+    public SteamVR_Action_Boolean m_FireAction = null;
+
+    public int m_Force = 10;
+
+    public Transform m_Barrel = null;
+
+    public GameObject m_ProjectilePrefab = null;
+
+    private SteamVR_Behaviour_Pose m_Pose = null;
+    private ProjectilePool m_ProjectilePool = null;
+
+    private void Awake()
+    {
+        m_Pose = GetComponentInParent<SteamVR_Behaviour_Pose>();
+
+        m_ProjectilePool = new ProjectilePool(m_ProjectilePrefab, 10);
+    }
+
+    private void Update()
+    {
+        if(m_FireAction.GetStateDown(m_Pose.inputSource))
+        {
+            Fire();
+        }
+    }
+
+    private void Fire()
+    {
+        Projectile targetProjectile = m_ProjectilePool.m_Projectiles[0];
+        targetProjectile.Launch(this);
+    }
+}
